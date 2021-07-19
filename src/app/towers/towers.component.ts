af62263290221f3a21b330b2b0b16c6814ae479f
Alan Sparrow <git@freeflight.org.uk>
2021-07-19 18:47:57 +0100

    Show towers in tower component

--- src/app/towers/towers.component.ts
+++ src/app/towers/towers.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
+import { Tower } from '../dove.service';
 
 @Component({
   selector: 'app-towers',
   templateUrl: './towers.component.html',
   styleUrls: ['./towers.component.css']
 })
-export class TowersComponent implements OnInit {
+export class TowersComponent implements OnChanges {
 
-  constructor() { }
+  @Input() towers: Tower[] = [];
 
-  ngOnInit(): void {
+  displayed: Tower[] = []
+
+  ngOnChanges(): void {
+    // Make big enough to show all the towers in Devon
+    this.displayed = this.towers.slice(0, 500);
   }
-
 }
